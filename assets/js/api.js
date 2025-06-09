860d0af7bd55050f26a74b45191f3016e5a7e1b3
murilois <murilomattosdev05@gmail.com>
2025-06-09 16:12:50 -0300

    Correção na api 2.0

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,5 +1,5 @@
 async function fetchProfileData() {
-    const url = './data/profile.json';
+    const url = '../../data/profile.json';
     const response = await fetch(url);
     return await response.json();
 }
