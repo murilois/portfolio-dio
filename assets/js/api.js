ade8f185090ad3597e2d5a44371cda46e1bd0a29
murilois <murilomattosdev05@gmail.com>
2025-06-09 15:59:51 -0300

    Correção na api

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,5 +1,5 @@
 async function fetchProfileData() {
-    const url = 'https://raw.githubusercontent.com/murilois/portfolio-dio/refs/heads/main/data/profile.json';
+    const url = './data/profile.json';
     const response = await fetch(url);
     return await response.json();
 }
